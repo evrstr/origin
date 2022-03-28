33cade4ad0e3c9451298fd46c2f0bcb3321b0d4a
yiqi <429427989@qq.com>
2022-03-29 00:04:45 +0800

    fixed

--- go.mod
+++ go.mod
@@ -1,13 +1,19 @@
 module github.com/duanhf2012/origin
 
-go 1.17
+go 1.18
 
 require (
 	github.com/go-sql-driver/mysql v1.5.0
-	github.com/golang/protobuf v1.4.3
+	github.com/gogo/protobuf v1.3.2
 	github.com/gomodule/redigo v1.8.3
 	github.com/gorilla/websocket v1.4.2
 	github.com/json-iterator/go v1.1.10
-	google.golang.org/protobuf v1.25.0
 	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
 )
+
+require (
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+)
+
+replace github.com/duanhf2012/origin => ../github.com/evrstr/origin
--- sysmodule/mongomodule/mongomodule_test.go
+++ sysmodule/mongomodule/mongomodule_test.go
@@ -2,38 +2,36 @@ package mongomodule
 
 import (
 	"fmt"
-	_ "gopkg.in/mgo.v2"
-	"gopkg.in/mgo.v2/bson"
 	"testing"
 	"time"
+
+	_ "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
-
-
 type Student struct {
-	ID    bson.ObjectId `bson:"_id"`
-	Name   string  `bson: "name"`
-	Age    int     `bson: "age"`
-	Sid    string  `bson: "sid"`
-	Status int     `bson: "status"`
+	ID     bson.ObjectId `bson:"_id"`
+	Name   string        `bson: "name"`
+	Age    int           `bson: "age"`
+	Sid    string        `bson: "sid"`
+	Status int           `bson: "status"`
 }
 
 type StudentName struct {
-	Name   string  `bson: "name"`
+	Name string `bson: "name"`
 }
 
-
 func Test_Example(t *testing.T) {
-	module:=MongoModule{}
-	module.Init("mongodb://admin:123456@192.168.2.119:27017",100, 5*time.Second,5*time.Second)
+	module := MongoModule{}
+	module.Init("mongodb://admin:123456@192.168.2.119:27017", 100, 5*time.Second, 5*time.Second)
 
 	// take session
-	s := module.Take()
+	s := module.Ref()
 	c := s.DB("test2").C("t_student")
 
 	//2.定义对象
 	insertData := Student{
-		ID:bson.NewObjectId(),
+		ID:     bson.NewObjectId(),
 		Name:   "seeta11",
 		Age:    35, //*^_^*
 		Sid:    "s20180907",
@@ -47,19 +45,18 @@ func Test_Example(t *testing.T) {
 		Status: 1,
 	}
 
-
 	//3.插入数据
 	err := c.Insert(&insertData)
 
 	//4.查找数据
-	selector := bson.M{"_id":bson.ObjectIdHex("5f25303e999c622d361989b0")}
-	m:=Student{}
+	selector := bson.M{"_id": bson.ObjectIdHex("5f25303e999c622d361989b0")}
+	m := Student{}
 	err = c.Find(selector).One(&m)
 
 	//5.更新数据
 	//selector2 := bson.M{"_id":bson.ObjectIdHex("5f25303e999c622d361989b0")}
 	updateData.ID = bson.ObjectIdHex("5f25303e999c622d361989b0")
-	err = c.UpdateId(bson.ObjectIdHex("5f25303e999c622d361989b0"),&updateData)
+	err = c.UpdateId(bson.ObjectIdHex("5f25303e999c622d361989b0"), &updateData)
 	if err != nil {
 		fmt.Print(err)
 	}
@@ -71,7 +68,7 @@ func Test_Example(t *testing.T) {
 	}
 
 	//7.序号自增
-	s.EnsureCounter("test2","t_student","5f252f09999c622d36198951")
+	s.EnsureCounter("test2", "t_student", "5f252f09999c622d36198951")
 	for i := 0; i < 3; i++ {
 		id, err := s.NextSeq("test2", "t_student", "5f252f09999c622d36198951")
 		if err != nil {
@@ -82,14 +79,13 @@ func Test_Example(t *testing.T) {
 	}
 
 	//8.setoninsert使用
-	info,uErr := c.Upsert(bson.M{"_id":bson.ObjectIdHex("5f252f09999c622d36198951")},bson.M{
-		"$setOnInsert":bson.M{"Name":"setoninsert","Age":55}})
-
+	info, uErr := c.Upsert(bson.M{"_id": bson.ObjectIdHex("5f252f09999c622d36198951")}, bson.M{
+		"$setOnInsert": bson.M{"Name": "setoninsert", "Age": 55}})
 
 	//9.修改部分数字数据
-	selector1 := bson.M{"_id":bson.ObjectIdHex("60473de655f1012e7453b369")}
-	update1 := bson.M{"$set":bson.M{"name":"xxxxx","age":1111}}
-	c.Update(selector1,update1)
+	selector1 := bson.M{"_id": bson.ObjectIdHex("60473de655f1012e7453b369")}
+	update1 := bson.M{"$set": bson.M{"name": "xxxxx", "age": 1111}}
+	c.Update(selector1, update1)
 
-	fmt.Println(info,uErr)
+	fmt.Println(info, uErr)
 }
